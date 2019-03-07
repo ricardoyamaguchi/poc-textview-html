d569028ff4fe5f154715ae936fce248a097e00bf
Ricardo Yamaguchi <ricardoy@ciandt.com>
2019-03-07 15:23:43 -0300

    Initial Commit

---
+++ poc-textview-html/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  poc-textview-html
+//
+//  Created by Ricardo Yamaguchi on 07/03/19.
+//  Copyright © 2019 Ricardo Yamaguchi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
